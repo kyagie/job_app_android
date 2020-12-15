41baf5fe33901d4002e645be2e28c4ac5ef2b639
Arthur Kyagulanyi <akyagulanyi@tayariafrica.com>
2020-12-15 19:02:41 +0300

    Color

--- app/src/main/java/com/example/ura_mobo_s18b23_291/JobDetailsActivity.java
+++ app/src/main/java/com/example/ura_mobo_s18b23_291/JobDetailsActivity.java
@@ -3,6 +3,8 @@ package com.example.ura_mobo_s18b23_291;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class JobDetailsActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class JobDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_details);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.job_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/ura_mobo_s18b23_291/PersonalDetailsActivity.java
+++ app/src/main/java/com/example/ura_mobo_s18b23_291/PersonalDetailsActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PersonalDetailsActivity extends AppCompatActivity {
 
@@ -12,9 +14,19 @@ public class PersonalDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_details);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.title_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
     public void jobDetails(View view) {
         Intent intent = new Intent(this, JobDetailsActivity.class);
         startActivity(intent);
     }
+
 }
