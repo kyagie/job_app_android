b2ec39cd9cb8c70ca7e8d4c3df6fb3890befcdeb
Arthur Kyagulanyi <akyagulanyi@tayariafrica.com>
2020-12-16 09:03:47 +0300

    Activity

--- app/src/main/java/com/example/ura_mobo_s18b23_291/JobDetailsActivity.java
+++ app/src/main/java/com/example/ura_mobo_s18b23_291/JobDetailsActivity.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
-
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.material.textfield.TextInputLayout;
 public class JobDetailsActivity extends AppCompatActivity {
+    private TextInputLayout filledDescTextField, filledQuaTextField;
+    private Button button3;
+    private AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,29 @@ public class JobDetailsActivity extends AppCompatActivity {
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        filledDescTextField = (TextInputLayout) findViewById(R.id.filledDescTextField);
+        filledQuaTextField = (TextInputLayout) findViewById(R.id.filledQuaTextField);
+
+        button3 = (Button) findViewById(R.id.button3);
+
+
+        awesomeValidation.addValidation(this, R.id.filledDescTextField,  "[A-Za-z0-9 _.,;!\"'/$]*", R.string.descerror);
+        awesomeValidation.addValidation(this, R.id.filledQuaTextField,  "[A-Za-z0-9 _.,;!\"'/$]*", R.string.descerror);
+
+        button3.setOnClickListener(this::feedBack);
+    }
+    private void submitForm() {
+        if (awesomeValidation.validate()) {
+            Intent intent = new Intent(this, feedbackActivity.class);
+            startActivity(intent);
+        }
     }
 
     public void feedBack(View view) {
-        Intent intent = new Intent(this, feedbackActivity.class);
-        startActivity(intent);
+        if (view == button3) {
+            submitForm();
+        }
     }
 }
